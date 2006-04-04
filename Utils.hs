cdcc67449f676e95ea1ede372710d58b983de60a
John Goerzen <jgoerzen@complete.org>
2006-04-04 16:16:34 -0500

    Initial add of Utils.hs
    darcs-hash:20060404211634-c2a52-675b921c8fa54263a45e59aa3ede9f77f0b5250c.gz

---
+++ Utils.hs
@@ -0,0 +1,16 @@
+{- dfsbuilt Utilities
+Copyright (c) 2006 John Goerzen
+Please see COPYRIGHT for more details
+-}
+
+module Utils where
+import System.Random
+
+getUniqueCDID :: IO String
+getUniqueCDID = 
+    do t <- getClockTime
+       random1 <- randomIO
+       random2 <- randomIO
+       return $ printf "DFS CD IMAGE, format 2, ID: %d,%d,%d\n"
+                (\(TOD x _ -> x) t) random1::Int random2::Int
+
