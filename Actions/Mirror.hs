4d148ccec774bd5dea0e4acedc44d10fd2517e28
John Goerzen <jgoerzen@complete.org>
2006-04-05 15:58:01 -0500

    Initial apt-move stuff
    darcs-hash:20060405205801-c2a52-ed2054d36a3e69d634267c694dd5b381274b408a.gz

--- Actions/Mirror.hs
+++ Actions/Mirror.hs
@@ -8,6 +8,8 @@ import Utils
 import MissingH.Cmd
 import MissingH.ConfigParser
 import System.Posix.Directory
+import System.IO
+import Text.Printf
 
 mirrorToWorkdir env repos =
     do im "Mirroring process starting."
@@ -18,8 +20,22 @@ mirrorToWorkdir env repos =
 
 procrepo env repo =
     do im $ "Running cdebootstrap for " ++ repo
+       -- First, download the packages.
        safeSystem "cdebootstrap" $
                   archargs ++ debugargs ++ ["-d", suite, targetdir, mirror]
+       -- Next, copy them into the mirror.
+       cfg <- openFile aptmovecfg WriteMode
+       hPrintf cfg "LOCALDIR=%s\n" mirrordir
+       hPrintf cfg "FILECACHE=%s/var/cache/bootstrap\n" targetdir
+       hPrintf cfg "LISTSTATE=%s/var/cache/bootstrap\n" targetdir
+       hPrintf cfg "DIST=%s\n" repo
+       case get (cp env) sect "arch" of
+         Left _ -> return ()
+         Right a -> hPrintf cfg "ARCH=%s\n" (a::String)
+       hPutStr cfg "COPYONLY=yes\nCONTENTS=yes\nAPTSITES=/all/\nPKGCOMP=\"none gzip\"\n"
+       hClose cfg
+       im $ "Running apt-move for " ++ repo
+       safeSystem "apt-move" ["-c", aptmovecfg, "update"]
     where
       targetdir = (wdir env) ++ "/target"
       mirrordir = (wdir env) ++ "/mirror"
@@ -32,4 +48,5 @@ procrepo env repo =
       debugargs = if isDebugging env
                      then ["--debug", "-v"]
                      else []
+      aptmovecfg = (wdir env) ++ "/apt-move.conf"
       
