6d88a26ec821459cdbcf2a98502dec341be424e1
John Goerzen <jgoerzen@complete.org>
2006-04-05 16:57:32 -0500

    Switched to reprepro
    darcs-hash:20060405215732-c2a52-7e7cf79896c5633f08693d3a31f86a2dfff8da6f.gz

--- Actions/Mirror.hs
+++ Actions/Mirror.hs
@@ -8,6 +8,9 @@ import Utils
 import MissingH.Cmd
 import MissingH.ConfigParser
 import System.Posix.Directory
+import Control.Exception
+import Data.List
+import MissingH.Path
 import System.IO
 import Text.Printf
 
@@ -24,18 +27,26 @@ procrepo env repo =
        safeSystem "cdebootstrap" $
                   archargs ++ debugargs ++ ["-d", suite, targetdir, mirror]
        -- Next, copy them into the mirror.
-       cfg <- openFile aptmovecfg WriteMode
-       hPrintf cfg "LOCALDIR=%s\n" mirrordir
-       hPrintf cfg "FILECACHE=%s/var/cache/bootstrap\n" targetdir
-       hPrintf cfg "LISTSTATE=%s/var/cache/bootstrap\n" targetdir
-       hPrintf cfg "DIST=%s\n" repo
-       case get (cp env) sect "arch" of
-         Left _ -> return ()
-         Right a -> hPrintf cfg "ARCH=%s\n" (a::String)
-       hPutStr cfg "COPYONLY=yes\nCONTENTS=yes\nAPTSITES=/all/\nPKGCOMP=\"none gzip\"\n"
-       hClose cfg
-       im $ "Running apt-move for " ++ repo
-       safeSystem "apt-move" ["-c", aptmovecfg, "update"]
+       mapM_ (\x -> handle (\_ -> return ()) (createDirectory x 0o755))
+                 [mirrordir, mirrordir ++ "/conf"]
+       safeSystem "touch" [mirrordir ++ "/conf/distributions"]
+       appendFile (mirrordir ++ "/conf/distributions") $ concat $ intersperse "\n" $
+           ["Origin: Debian",
+            "Label: Debian",
+            "Suite: " ++ repo,
+            "Codename: " ++ repo,
+            "Version: 0.dfs",
+            "Architectures: alpha amd64 arm hppa i386 ia64 m68k mips mipsel powerpc s390 sparc",
+            "Description: Debian From Scratch cache of " ++ repo,
+            "Components: main non-free contrib",
+            "\n\n\n"]
+       im $ "Running reprepro for " ++ repo
+       bracketCWD mirrordir $
+         safeSystem "bash"
+           ["-c", 
+            "for INFILE in " ++ targetdir ++ "/var/cache/bootstrap/*.deb; do "
+            ++ "reprepro " ++ repdebugargs ++ " -b . includedeb " ++ suite ++
+            " \"$INFILE\"; done"]
     where
       targetdir = (wdir env) ++ "/target"
       mirrordir = (wdir env) ++ "/mirror"
@@ -48,5 +59,8 @@ procrepo env repo =
       debugargs = if isDebugging env
                      then ["--debug", "-v"]
                      else []
+      repdebugargs = if isDebugging env
+                        then "-V"
+                        else ""
       aptmovecfg = (wdir env) ++ "/apt-move.conf"
       
