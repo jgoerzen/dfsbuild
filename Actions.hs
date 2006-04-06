702fa2ea904982f0ecb940b021a12b9ea13c98ec
John Goerzen <jgoerzen@complete.org>
2006-04-05 22:01:47 -0500

    Wrote installpkgs
    darcs-hash:20060406030147-c2a52-9379f284c3e41ed6fbbbf1fc96df33d1de814b67.gz

--- Actions.hs
+++ Actions.hs
@@ -20,6 +20,9 @@ run env =
 
        -- Now bootstrap the CD image
        cdebootstrap env
+
+       -- Install additional packages
+       installpkgs env
        
 dlMirrors env = 
     do let suites = splitWs $ eget env "dlrepos"
@@ -34,5 +37,27 @@ cdebootstrap env =
                  "deb " ++ (eget env "mirror") ++ " " ++ (eget env "suite")
                  ++ " main\n"
        dm $ "Moving mirror to /opt/packages on target"
-       renameFile ((wdir env) ++ "/mirror") (targetdir ++ "/opt/packages")
-    where targetdir = (wdir env) ++ "/target"
+       renameFile ((wdir env) ++ "/mirror") 
+                      ((targetdir env) ++ "/opt/packages")
+
+installpkgs env =
+    do im "Installing additional packages."
+
+       -- Set debconf priority to critical so that user doesn't get prompted
+       safeSystem "chroot" [targetdir env, "/bin/bash", "-c",
+         "echo \"debconf\tdebconf/priority\tselect\tcritical\" | debconf-set-selections"]
+    
+       -- Copy resolv.conf so apt-get update/install works
+       safeSystem "cp" ["/etc/resolv.conf", targetdir env ++ "/etc"]
+
+       -- Now do apt-get
+       safeSystem "chroot" [targetdir env, "apt-get", "update"]
+       safeSystem "chroot" $ 
+                      [targetdir env, "apt-get", "-y", "install"] ++ pkgs
+
+       -- And remove the resolv.conf again
+       removeFile (targetdir env ++ "/etc/resolv.conf")
+
+       safeSystem "chroot" [targetdir env, "apt-get", "clean"]
+
+    where pkgs = splitWs (eget "packages")
