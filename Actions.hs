9aeae15893886fc94735d9312689c2f8cc9a836e
John Goerzen <jgoerzen@complete.org>
2006-04-05 19:25:57 -0500

    Wrote cdebootstrap support
    darcs-hash:20060406002557-c2a52-1d790bcdcbcfbf9fa2e316796d7b1c0613600408.gz

--- Actions.hs
+++ Actions.hs
@@ -14,8 +14,25 @@ run env =
        mapM_ (createDirectory `flip` 0o755) 
              [imagedir env, imagedir env ++ "/opt",
               imagedir env ++ "/opt/dfsruntime"]
+
+       -- Download all suites that we'll be using
        dlMirrors env
+
+       -- Now bootstrap the CD image
+       cdebootstrap env
        
 dlMirrors env = 
     do let suites = splitWs $ eget env "dlrepos"
        Actions.Mirror.mirrorToWorkdir env suites
+
+cdebootstrap env =
+    do im $ "Bootstrapping into " ++ targetdir
+       safeSystem "cdebootstrap" [eget env "suite", target,
+                                  "file://" ++ (wdir env) ++ "/mirror"]
+       dm $ "Saving sources.list"
+       writeFile (targetdir ++ "/etc/apt/sources.list") $
+                 "deb " ++ (eget env "mirror") ++ " " ++ (eget env "suite")
+                 ++ " main\n"
+       dm $ "Moving mirror to /opt/packages on target"
+       renameFile ((wdir env) ++ "/mirror") (targetdir ++ "/opt/packages")
+    where targetdir = (wdir env) ++ "/target"
